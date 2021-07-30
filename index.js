abd8e6fcacf09bdd378a24846635642c98c4a09c
Silas Getachew <silogecho@gmail.com>
2021-07-30 15:08:09 +0300

    feat : GraphQL start

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require("dotenv").config()
 const express = require('express')
 const helmet = require('helmet');
 const morgan = require('morgan')
+const expressGraphQL = require('express-graphql')
+const { GraphQLSchema, GraphQLObjectType } = require("graphql")
 const cors = require('cors')
 const connectMongose = require('./db_connection');
 const routes = require('./src/routes')
 const app = express();
 const PORT = process.env.PORT || 8585
+
+const schema = new GraphQLSchema({
+    query: {}
+})
 
 /**
  * Express middlerware to halder json data
@@ -19,6 +25,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use('/graphql', expressGraphQL({ graphql: true }))
 /**
  * DB connection,MONGO DB,CONNECTION POOL
  */
